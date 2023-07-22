3ce96c1732c9fdac56207aad5f126e543983b424
James Kim <jakim929@gmail.com>
2023-07-22 11:42:00 -1000

    add multicall support for minting

--- src/components/ClaimInvite.tsx
+++ src/components/ClaimInvite.tsx
@@ -1,6 +1,8 @@
-import { Address, Hex } from 'viem'
+import { Address, Hex, encodeFunctionData } from 'viem'
 import { useContractWrite, usePrepareContractWrite } from 'wagmi'
 import { OptimistInviter } from '../contracts/OptimistInviter.sol'
+import { Optimist } from '../contracts/Optimist.sol'
+import { Multicall3 } from '../contracts/Multicall3.sol'
 
 export const ClaimInvite = ({
   inviter,
@@ -15,12 +17,32 @@ export const ClaimInvite = ({
   nonce: Hex
   onSubmit: (hash: Hex) => void
 }) => {
-  const { config, error } = usePrepareContractWrite({
+  const claimInviteCallData = encodeFunctionData({
     ...OptimistInviter.write().claimInvite(
       recipient,
       { issuer: inviter, nonce },
       signature,
     ),
+  })
+
+  const mintNFTCallData = encodeFunctionData({
+    ...Optimist.write().mint(recipient),
+  })
+
+  const { config, error } = usePrepareContractWrite({
+    ...Multicall3.write().aggregate3([
+      {
+        target: OptimistInviter.addresses[10],
+        allowFailure: false,
+        callData: claimInviteCallData,
+      },
+      {
+        target: Optimist.addresses[10],
+        allowFailure: false,
+        callData: mintNFTCallData,
+      },
+    ]),
+    value: 0n,
   })
 
   const { data, writeAsync } = useContractWrite(config)
@@ -36,7 +58,7 @@ export const ClaimInvite = ({
           }
         }}
       >
-        Claim invite for recipient
+        Claim invite and mint for recipient
       </button>
     </div>
   )
